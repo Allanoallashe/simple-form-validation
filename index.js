88325f41b8c3999283682a77a914988720a61b36
Allan <owinoallan77@gmail.com>
2023-04-19 21:09:54 +0300

    perfect js form

--- index.js
+++ index.js
@@ -4,23 +4,75 @@ const email = document.getElementById('email');
 const pwdOne = document.getElementById('pwd1');
 const pwdTwo = document.getElementById('pwd2');
 const phone = document.getElementById('phone');
-const form = document.querySelector('form');
+
+function Validation() {
+  // check an empty user name
+  if (userName.value.trim() === "") {
+    Invalid(userName,"User Name Is Required!");
+  }
+  else {
+    Successful(userName);
+  }
+  // email
+  if (email.value.trim() === "") {
+    Invalid(email,"Enter Your Email!");
+  }
+  else {
+    if (!validEmail(email.value.trim())) {
+      Invalid(email,"Invalid Email!");
+    } else 
+      Successful(email);
+  }
+  // password1
+   if (pwdOne.value.trim() === "") {
+    Invalid(pwdOne,"Password1 Is Required!");
+  }
+  else {
+    Successful(pwdOne);
+  }
+  // Password2
+   if (pwdTwo.value.trim() === "") {
+    Invalid(pwdTwo,"Password2 Is Required!");
+  }
+   else {
+     if (pwdTwo.value.trim() !== pwdOne.value.trim()) {
+       Invalid(pwdTwo, "Passwords do not Match!");
+     } else {
+       Successful(pwdTwo);
+     }
+  }
+  // telephone
+   if (phone.value.trim() === "") {
+    Invalid(phone,"Telephone Is Required!");
+  }
+  else {
+    Successful(phone);
+  }
+}
+
+function Successful(input) {
+  let parent = input.parentElement;
+  let message = parent.querySelector('section');
+  message.style.visibility = "hidden";
+  parent.classList.add('valid');
+  parent.classList.remove('empty');
+}
+function Invalid(input,alert) {
+  let parent = input.parentElement;
+  let message = parent.querySelector('section');
+  message.style.visibility = "visible";
+  message.innerText = alert;
+  message.style.color = "#e41d16";
+    parent.classList.add('empty');
+  parent.classList.remove('valid');
+}
+function validEmail(email)
+{
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
 
 document.querySelector('.submit').addEventListener('click', (event) => {
   event.preventDefault();
-  if (userName.value.trim() === "") {
-    Empty(userName, "User Name Required");
-  } else {
-    Valid(userName);
-  }
+  Validation();
 });
-
-function Empty(input, message) {
-  document.querySelector('section').style.visibility = "visible";
-  document.querySelector('section').innerText=message;
-}
-function Valid(input) {
-  document.querySelector('section').style.visibility = "hidden";
-  document.querySelector('section').innerText="";
-}
 
